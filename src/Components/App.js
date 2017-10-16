79661ccb7e11f5fbafe92dd16bce6f14dc8b2256
pandaman <aeonzamaek@gmail.com>
2017-10-16 22:26:38 +0200

    Refactored a few stuffs

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,1 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import ResponsiveDrawer from './Drawer';
+
+import store from '../store/configureStore'
+
+import '../App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+        <Provider store={store}>
+            <ResponsiveDrawer />
+        </Provider>
+    );
+  }
+}
+
+
+export default App;
--- src/container/UsersList.js
+++ src/container/UsersList.js
@@ -11,7 +11,8 @@ const mapStateToProps = state => {
 }
 
 // const mapDispatchToProps = dispatch => {
-// 	return {	
+// 	return {
+//         // ?  		
 // 	}
 // }
 
